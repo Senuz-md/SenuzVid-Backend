445405eaf814284c079d393662622cdb3431999f
Senuz-md <Seneth049@gmail.com>
2025-12-16 08:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-console.log("BOOT OK");
-
 const http = require("http");
 
 const port = process.env.PORT || 3000;
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("HEROKU WORKING");
-  })
-  .listen(port, () => {
-    console.log("Listening on " + port);
-  });
+http.createServer((req, res) => {
+  res.end("HEROKU OK");
+}).listen(port);
